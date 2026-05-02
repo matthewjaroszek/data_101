aa54f829f73943e0d1c70b2c1dca176ea04d83a4
matthewjaroszek <matthewj369@icloud.com>
2026-05-01 20:56:12 -0400

    update

--- matt.R
+++ matt.R
@@ -1,6 +1,7 @@
 options(repos = c(CRAN = "https://cloud.r-project.org"))
 install.packages("httpgd")
 df <- read.csv("us_power_data.csv")
+#2001/01 - 2024/01
 
 avgs <- df |>
   group_by(year, month, sectorName) |>
@@ -12,26 +13,48 @@ avgs <- df |>
   ) |>
   mutate(date = paste(year, month, sep = "/"))
 
-avgs <- avgs |>
-  select(date, year, month, sectorName, everything())
-print(colnames(avgs))
-
+# make sure BOTH data frames have real Date columns
 avgs$date <- as.Date(paste0(avgs$date, "/1"), format = "%Y/%m/%d")
-sectors <- avgs[avgs$sectorName == "residential", ]
+df$date   <- as.Date(paste(df$year, df$month, "1", sep = "-"))
 
-start_date <- as.Date("2022-01-01")
+chosen_sector <- "residential"
+
+us_avg <- subset(avgs, tolower(trimws(sectorName)) == chosen_sector)
+us_avg <- subset(us_avg, !is.na(date) & !is.na(price))
+
+nj <- subset(df,
+             tolower(trimws(sectorName)) == chosen_sector &
+             stateDescription == "New Jersey")
+nj <- subset(nj, !is.na(date) & !is.na(price))
+
+mn <- subset(df,
+             tolower(trimws(sectorName)) == chosen_sector &
+             stateDescription == "Minnesota")
+mn <- subset(mn, !is.na(date) & !is.na(price))
+
+start_date <- as.Date("2001-01-01")
 end_date   <- as.Date("2024-12-01")
+x_ticks <- seq(start_date, end_date, by = "1 year")
 
-x_ticks <- seq(start_date, end_date, by = "1 months")
-
-plot(sectors$date, sectors$price,
+plot(us_avg$date, us_avg$price,
      type = "b",
+     col = "black",
+     pch = 19,
+     lwd = 2,
      xlab = "Date",
      ylab = "Price",
      main = "Date vs Price",
-     pch = 19,
-     cex = 0.8,
      xlim = c(start_date, end_date),
      xaxt = "n")
 
-axis(1, at = x_ticks, labels = format(x_ticks, "%Y/%m"))
+lines(nj$date, nj$price, type = "b", col = "red", pch = 19, lwd = 2)
+lines(mn$date, mn$price, type = "b", col = "blue", pch = 19, lwd = 2)
+
+axis(1, at = x_ticks, labels = format(x_ticks, "%Y"))
+
+legend("topleft",
+       legend = c("US Avg", "New Jersey", "Minnesota"),
+       col = c("black", "red", "blue"),
+       lty = 1,
+       pch = 19,
+       lwd = 2)
